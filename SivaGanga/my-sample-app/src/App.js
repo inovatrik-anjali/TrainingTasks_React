38c7026d990863340cc4fc177aaa4ed0b9ee580b
Sivaganga <sivaganga@inovatrik.com>
2025-10-01 17:22:54 +0530

    about Updated

--- SivaGanga/my-sample-app/src/App.js
+++ SivaGanga/my-sample-app/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import HeroSection from './components/HeroSection';
+import About from './components/About';
+
+
 
 function App() {
   return (
     <div>
+      <Header />
+      <HeroSection />
+      <About />
 
-      <Footer/>
 
     </div>
   );
